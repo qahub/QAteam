c1d2aa92b3cae48e41c31324a955fbf6c63d23a4
goldmedal <liugs963@gmail.com>
2014-05-13 22:56:44 +0800

    debug

--- qa_cgi/loadQAData.php
+++ qa_cgi/loadQAData.php
@@ -11,17 +11,16 @@
 	 * */
 
 	include("connect.php");
-
 	$topic = $_GET['topic'];
 	$count = $_GET['count'];
 	$fration = $_GET['fration'];
 	$tableName = "5_qaData".$topic; 
 	$counter  = 0;
-	$dataQuery = mysql_query("SELECT `title`, `content` FROM `5_qaData$topic` WHERE `id` > '$count' AND `fration` = '$fration'");
-	$dataRow = mysql_fetch_assoc($dataQuery));
+	$dataQuery = mysql_query("SELECT `title`, `content` FROM `$tableName` WHERE `id` > '$count' AND `fration` = '$fration'") or die(mysql_error());
+	$dataRow = mysql_fetch_assoc($dataQuery) or die(mysql_error());
 	$jsonData['title'] = $dataRow['title'];
 	$jsonData['content'] = $dataRow['content'];
 	$json = json_encode($jsonData);
 
-	echo $json;
+	echo $json
 ?>
