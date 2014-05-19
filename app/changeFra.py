b48dd7d33aba3ab88d5003d2a238943c99805e6e
goldmedal <liugs963@gmail.com>
2014-05-20 01:36:07 +0800

    count Time

--- app/changeFra.py
+++ app/changeFra.py
@@ -1,13 +1,41 @@
 import MySQLdb
 import time
-import datetime
+from datetime import datetime
 
 db = MySQLdb.connect(host="merry.ee.ncku.edu.tw", user="qateam", passwd="qateam01", db="qateam")
 cursor = db.cursor()
 
+now = datetime.now()
+standTime = now.second
+
+while (standTime % 15) != 0:
+	standTime += 1
+	if standTime == 60:
+		standTime = 0
+
+print standTime
+
 while True:
-	t = time.time()
-	t = time.strftime('%S',time.localtime(t))
-	print t
-	if t==50:
-		print 'yes'
+
+	now = datetime.now()
+	print now.second
+
+	if now.second == standTime:
+		cursor.execute("SELECT * FROM `5_NuclearState` WHERE `name` = 'nowFration'")
+		result = cursor.fetchall()
+		for row in result:
+			nowState = row[1]
+		if nowState == 0:
+			cursor.execute("UPDATE `5_NuclearState` SET `value` = '1' WHERE `name` = 'nowFration'")
+			db.commit()
+			print "turn to 1"
+		else:
+			cursor.execute("UPDATE `5_NuclearState` SET `value` = '0' WHERE `name` = 'nowFration'")
+			db.commit()
+			print "turn to 0"
+		
+		standTime =  standTime + 15
+		if standTime == 60:
+			standTime = 0
+
+	time.sleep(1);
