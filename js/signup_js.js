daf79f49ced5fa3e6a1ab8fc87bfe2c683e03370
Mouther <za7788az@hotmail.com>
2014-05-21 03:54:08 +0800

    spin test

--- js/signup_js.js
+++ js/signup_js.js
@@ -49,7 +49,7 @@ function checkUser(){
 			console.log(thrownError);
 		},
 		complete: function() {
-
+			startspin();
 		}
 
 	});
@@ -77,7 +77,7 @@ function firstLogin(_uid, _username){
 
 		},
 		complete: function() {
-
+			startspin()
 		}
 
 
@@ -170,3 +170,27 @@ $(document).ready( function(){
 	});
 
 });
+
+function startspin(){
+
+	var opts = {
+	  lines: 17, // The number of lines to draw
+	  length: 35, // The length of each line
+	  width: 29, // The line thickness
+	  radius: 55, // The radius of the inner circle
+	  corners: 0.9, // Corner roundness (0..1)
+	  rotate: 82, // The rotation offset
+	  direction: 1, // 1: clockwise, -1: counterclockwise
+	  color: '#000', // #rgb or #rrggbb or array of colors
+	  speed: 2.2, // Rounds per second
+	  trail: 60, // Afterglow percentage
+	  shadow: false, // Whether to render a shadow
+	  hwaccel: false, // Whether to use hardware acceleration
+	  className: 'spinner', // The CSS class to assign to the spinner
+	  zIndex: 2e9, // The z-index (defaults to 2000000000)
+	  top: '50%', // Top position relative to parent
+	  left: '50%' // Left position relative to parent
+	};
+	var target = document.getElementById('container');
+	var spinner = new Spinner(opts).spin(target);
+}
