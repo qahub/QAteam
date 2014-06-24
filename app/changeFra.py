2ddde031d0b168db1d655cb976e72a6d24fff55b
goldmedal <liugs963@gmail.com>
2014-06-24 21:22:44 +0800

    round controller

--- app/changeFra.py
+++ app/changeFra.py
@@ -1,56 +1,130 @@
 import MySQLdb
+import MySQLdb.cursors
 import time
 from datetime import datetime
 
-db = MySQLdb.connect(host="merry.ee.ncku.edu.tw", user="qateam", passwd="qateam01", db="qateam")
+def getState(cursor, db) :
+
+	cursor.execute("SELECT * FROM `5_NuclearState`")
+	result = cursor.fetchall()
+	dict = {}
+	for row in result :
+		name = dict['name']
+		value = dict['value']
+		dict[name] = value
+
+	return dict
+
+def setState(cursor, db, name, value) :
+
+	cursor.execute("UPDATE `5_NuclearState` SET `value` = '"+value+"' WHERE `name` = '"+name+"'")
+	db.commit()
+
+
+
+def create_new_round(nowFra, nowTable, cursor,db) :
+
+	if nowFra == 1 :
+		nowTable = nowFra
+		nowFration = "White"
+	else :
+		nowTable = nowFra
+		nowFration = "Black"
+
+	qTableName = "5_"+nowFration+"NuclearQ"+nowTable
+	aTableName = "5_"+nowFration+"NuclearA"+nowTable
+
+	qsql = "CREATE TABLE `"+qTableName+"`(id int(4) auto_increment primary key, uid int(100), comment longtext, grade int(1) DEFAULT 0, date date, time time, username varchar(20), evaluate mediumtext)"
+	asql = "CREATE TABLE `"+aTableName+"`(id int(4) auto_increment primary key, uid int(100), comment longtext, grade int(1) DEFAULT 0, date date, time time, username varchar(20), evaluate mediumtext)"
+
+	cursor.execute(qsql)
+	db.commit()
+	cursor.execute(asql)
+	db.commit()
+
+
+def output_and_change_fration(cursor,db) :
+
+	row = getState(cursor,db)
+
+	if row['nowFration'] == 1 :
+		nowTable = row['whiteNum']
+		nowFration = "White"
+	else :
+		nowTable = row['blackNum']
+		nowFration = "Black"
+
+	nowFraNum = row['nowFration']
+	nowStatus = "A"
+
+	tableName = "5_" + nowFration + "Nuclear" + nowStatus + nowTable;
+	sql = "SELECT * FROM `"+tableName"` ORDER BY `grade` DESC")
+	cursor.execute(sql)
+	result = cursor.fetchone()
+	contentA = result['comment']
+	
+	nowStatus = "Q"
+	tableName = "5_" + nowFration + "Nuclear" + nowStatus + nowTable;
+	sql = "SELECT * FROM `"+tableName"` ORDER BY `grade` DESC")
+	cursor.execute(sql)
+	result = cursor.fetchone()
+	contentQ = result['comment']
+
+	cursor.execute("INSERT INTO `5_qaDataNuclear`(`contentA`, ,`contentQ`,`fration`) VALUES('"+str(contentA)+"','"+str(contentQ)+"', '"+nowFraNum+"')")
+	db.commit()
+	
+	nowTalbe += 1
+
+	if nowFraNum == 2 :
+		cursor.execute("UPDATE `5_NuclearState` SET `value` = '"+nowTable+"' WHERE `name` = 'blackNum'")
+		db.commit()
+		cursor.execute("SELECT * FROM `5_NuclearState` WHERE `name` = 'whiteNum'")
+		result = cursor.fetchone()
+		nowTable = result['valeu']
+		nowFraNum = 1
+	else :
+		cursor.execute("UPDATE `5_NuclearState` SET `value` = '"+nowTable+"' WHERE `name` = 'whiteNum'")
+		db.commit()
+		cursor.execute("SELECT * FROM `5_NuclearState` WHERE `name` = 'blackNum'")
+		result = cursor.fetchone()
+		nowTable = result['valeu']
+		nowFraNum = 2
+
+	cursor.execute("UPDATE `5_NuclearState` SET `value` = '"+nowFraNum+"' WHERE `name` = 'nowFration'")
+	db.commit()
+	
+	create_new_round(nowFraNum, nowTable, cursor, db)
+
+def change_mode(cursor, db) :
+
+	row = getState(cursor, db)
+	if cmp(str(row['nowStatus']), "Q") :
+		setState(cursor, db, nowStatus, "A")
+	else if cmp(str(row['nowStatus']), "A") :
+		setState(cursor, db, nowStatus, "Q")
+
+
+db = MySQLdb.connect(host="merry.ee.ncku.edu.tw", user="qateam", passwd="qateam01", db="qateam", cursorclass=MySQLdb.cursors.DictCursor)
 cursor = db.cursor()
 
-now = datetime.now()
-standTime = now.second
+#now = datetime.now()
+#standTime = now.second
+qTime = 60  #討論回答時間
+atime = 60  #討論問題時間
+wikiTime = 10 #共筆時間
 
-while (standTime % 15) != 0:
-	standTime += 1
-	if standTime == 60:
-		standTime = 0
 
-print standTime
+#while (standTime % qTime) != 0:
+#	standTime += 1
+#	if standTime == 60:
+#		standTime = 0
+
+#print standTime
 
 while True:
 
-	now = datetime.now()
-	print now.second
+	time.sleep(qTime)
+	change_mode(cursor, db)
 
-	if now.second == standTime:
-		cursor.execute("SELECT * FROM `5_NuclearState` WHERE `name` = 'nowFration'")
-		result = cursor.fetchall()
-		for row in result:
-			nowState = row[1]
-		if nowState == 2:
-			cursor.execute("SELECT * FROM `5_BlackNuclear1` ORDER BY `grade` DESC")
-			row = cursor.fetchone();
-			comment = row[2]
-			print comment	
-
-			cursor.execute("INSERT INTO `5_qaDataNuclear`(`content`, `fration`) VALUES('"+str(comment)+"', '2')")
-			db.commit()
-
-			cursor.execute("UPDATE `5_NuclearState` SET `value` = '1' WHERE `name` = 'nowFration'")
-			db.commit()
-			print "turn to 1"
-		else:
-			cursor.execute("SELECT * FROM `5_WhiteNuclear1` ORDER BY `grade` DESC")
-			row = cursor.fetchone();
-			comment = row[3]
-			print comment	
-			cursor.execute("INSERT INTO `5_qaDataNuclear`(`content`, `fration`) VALUES('"+str(comment)+"', '1')")
-			db.commit()
-
-			cursor.execute("UPDATE `5_NuclearState` SET `value` = '2' WHERE `name` = 'nowFration'")
-			db.commit()
-			print "turn to 2"
-		
-		standTime =  standTime + 15
-		if standTime == 60:
-			standTime = 0
-
-	time.sleep(1);
+	time.sleep(aTime)
+	output_and_change_fration(cursor, db)
